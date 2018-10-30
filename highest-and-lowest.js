0c3a25a45e3bf4ecf15c69f83766695c56c61ff0
Nora <norapuskas@yahoo.com>
2018-10-30 14:39:43 +0100

    return highest and lowest number as a string

---
+++ highest-and-lowest.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let testArray = '4 5 29 54 4 0 -214 542 -64 1 -3 6 -6';
+
+function highAndLow(numbers) {
+  let result = '';
+  let numbersList = numbers.split(' ');
+  let max = Math.max(...numbersList).toString();
+  let min = Math.min(...numbersList).toString();
+  return (result += max + ' ' + min);
+}
+
+console.log(highAndLow(testArray));
