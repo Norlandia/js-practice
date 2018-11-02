8dd87f6d0b42a0748202e3eddc7dced2c166aa29
Nora <norapuskas@yahoo.com>
2018-11-02 18:57:25 +0100

    fix

--- count-numbers.js
+++ count-numbers.js
@@ -2,13 +2,12 @@
 
 let testArray = [3, 5, 6, 3, 1, 1, 5];
 
-const getSum= (list) => {
+const getSum = (list) => {
   let numberFrequency = {};
 
   list.forEach((number) => {
     (numberFrequency[number]) ? numberFrequency[number]++ : numberFrequency[number] = 1;
   });
   return numberFrequency;
-}
-
+};
 console.log(getSum(testArray));
