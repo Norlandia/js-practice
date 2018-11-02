9f58b81dbf0f256da6db3d03725bd051c997872c
Nora <norapuskas@yahoo.com>
2018-11-02 20:24:31 +0100

    fix style

--- reverse.js
+++ reverse.js
@@ -5,7 +5,6 @@ let letters = ['n', 'o', 'r', 'l', 'a', 'n', 'd', 'i', 'a'];
 const reverse = (elements) => {
   for (let i = elements.length - 1; i >= 0; i--) {
     console.log(elements[i]);
-    
   }
 };
 
