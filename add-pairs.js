b6270b63a16347bce25ed056b0a39646cb8c4b04
Nora <norapuskas@yahoo.com>
2018-11-02 18:43:41 +0100

    refactor

--- add-pairs.js
+++ add-pairs.js
@@ -1,28 +1,19 @@
 'use strict';
 
-let myList = [1, 2, 3, 4, 5];
+let myList = [1, 2, 3, 4, 6];
 
 function addPairs(numbers) {
   let result = [];
 
-  if (numbers.length % 2 === 0) {
-    for (let i = 0; i < numbers.length; i++) {
-      if (i % 2 === 0) {
-        result.push(numbers[i] + numbers[i + 1]);
-      }
+  for (let i = 0; i < numbers.length; i++) {
+    if (i % 2 === 0 && numbers[i + 1]) {
+      result.push(numbers[i] + numbers[i + 1]);
+    } else if (i % 2 === 0) {
+      result.push(numbers[i]);
     }
-    return result;
-  } else {
-    let last = numbers[numbers.length - 1];
-    numbers.pop();
-    for (let i = 0; i < numbers.length; i++) {
-      if (i % 2 === 0) {
-        result.push(numbers[i] + numbers[i + 1]);
-      }
-    }
-    result.push(last);
-    return result;
   }
+
+  return result;
 }
 
 console.log(addPairs(myList));
