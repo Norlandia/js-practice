80386ce1c05573c1a818876020ac5c965a5111df
Nora <norapuskas@yahoo.com>
2018-11-06 22:08:34 +0100

    create isIsogram function

---
+++ isogram.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let testWord = 'Dermatoglyphics';
+let testWord2 = 'moOse';
+let testWord3 = '';
+
+function isIsogram(word) {
+  let letterObject = {};
+
+  word
+    .toLowerCase()
+    .split('')
+    .map((letter) => {
+      if (letterObject[letter]) {
+        letterObject[letter]++;
+      } else {
+        letterObject[letter] = 1;
+      }
+    });
+
+  for (let amount of Object.values(letterObject)) {
+    if (amount > 1) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+console.log(isIsogram(testWord3));
