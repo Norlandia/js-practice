b86bc01cd18bb79c31e83d764b0675a1a6d06f70
Nora <norapuskas@yahoo.com>
2018-11-13 14:32:46 +0100

    weird refactoring of a for loop

--- sum-numbers-until.js
+++ sum-numbers-until.js
@@ -5,9 +5,7 @@ let testArray = [1, 2, 3, 4, 5];
 function getSum(list, limit) {
   let sum = 0;
 
-  for (let i = 0; i < limit; i++) {
-    sum += list[i % 5];
-  }
+  for (let i = 0; i < limit; sum += list[i++ % 5]);
   return sum;
 }
 
