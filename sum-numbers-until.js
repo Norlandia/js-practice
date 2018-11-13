2116cb638cefbeab39eded76c6e723eb33cc2729
Nora <norapuskas@yahoo.com>
2018-11-13 14:30:10 +0100

    refactor sum-numbers-until solution

--- sum-numbers-until.js
+++ sum-numbers-until.js
@@ -4,14 +4,9 @@ let testArray = [1, 2, 3, 4, 5];
 
 function getSum(list, limit) {
   let sum = 0;
-  let roundCount = 0;
 
-  for (let i = 0; roundCount < limit; i++) {
-    if (i === list.length) {
-      i = 0;
-    }
-    sum += list[i];
-    roundCount += 1;
+  for (let i = 0; i < limit; i++) {
+    sum += list[i % 5];
   }
   return sum;
 }
