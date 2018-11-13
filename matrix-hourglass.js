165b768fcd4c5541df6b018957cbd68bc3020a65
Nora <norapuskas@yahoo.com>
2018-11-13 21:42:03 +0100

    refactor, remove if statement

--- matrix-hourglass.js
+++ matrix-hourglass.js
@@ -1,14 +1,13 @@
 'use strict';
 
 let testMatrix = [
-  [1 ,1 ,1 ,0 ,0 ,0],
-  [0 ,1 ,0 ,0 ,0 ,0],
-  [1 ,1 ,1 ,0 ,0 ,0],
-  [0 ,0 ,2 ,4 ,4 ,0],
-  [0 ,0 ,0 ,2 ,0 ,0],
-  [0 ,0 ,1 ,2 ,4 ,0]
+  [1, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0],
 ];
-
 
 //counts sum of the hourglass shape in athe 3x3 matrix
 const hourglassSum = (smallMatrix) => {
@@ -28,31 +27,26 @@ const makeSmallMatrix = (largeMatrix, x, y) => {
   for (let i = x; i <= x + 2; i++) {
     let row = [];
     for (let j = y; j <= y + 2; j++) {
-      row.push(largeMatrix[i][j])
+      row.push(largeMatrix[i][j]);
     }
     small.push(row);
   }
-  return small;  
-}
-
-
+  return small;
+};
 
 //moves 3x3 hg, adds sums to array, returns largest sum
 const getLargestSum = (largeMatrix) => {
   let hourglassSumList = [];
 
-  for (let i = 0; i < largeMatrix.length; i++) {
-    for (let j = 0; j < largeMatrix[i].length; j++) {
-      if (largeMatrix[i + 2] && largeMatrix[i + 2][j + 2]) {
-        let smallMatrix = makeSmallMatrix(largeMatrix, i, j);
-        let smallMatrixSum = hourglassSum(smallMatrix);
-        hourglassSumList.push(smallMatrixSum); 
-      }
+  for (let i = 0; i < largeMatrix.length - 2; i++) {
+    for (let j = 0; j < largeMatrix[i].length - 2; j++) {
+      let smallMatrix = makeSmallMatrix(largeMatrix, i, j);
+      let smallMatrixSum = hourglassSum(smallMatrix);
+      hourglassSumList.push(smallMatrixSum);
     }
   }
 
   return Math.max(...hourglassSumList);
-}
+};
 
 console.log(getLargestSum(testMatrix));
-
