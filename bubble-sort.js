57585fab1162aa50879e08b0cf1f3f4e367a5428
Nora <norapuskas@yahoo.com>
2018-11-15 22:05:47 +0100

    refactor

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,24 +2,29 @@
 
 let testList = [3, 2, 1];
 
-function countSwaps(list) {
+function swap(array, j) {
   let temp = 0;
-  let swap = 0;
+
+  temp = array[j];
+  array[j] = array[j + 1];
+  array[j + 1] = temp;
+}
+
+function countSwaps(list) {
+  let swapCount = 0;
   for (let i = 0; i < list.length; i++) {
     for (let j = 0; j < list.length - 1; j++) {
       // Swap adjacent elements if they are in decreasing order
       if (list[j] > list[j + 1]) {
-        // swap(list[j], list[j + 1]);
-        temp = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = temp;
-        swap++;
+        swap(list, j);
+
+        swapCount++;
       }
     }
   }
-  console.log(`Array is sorted in ${swap} swaps.`);
+  console.log(`Array is sorted in ${swapCount} swaps.`);
   console.log(`First Element: ${list[0]}`);
-  console.log(`Last Element: ${list[list.length -1]}`);
+  console.log(`Last Element: ${list[list.length - 1]}`);
 }
 
 countSwaps(testList);
