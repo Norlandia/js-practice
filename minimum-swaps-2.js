bc70b5337ae8c42504164cbc231a19c4fc3f3549
Nora <norapuskas@yahoo.com>
2018-11-16 19:22:30 +0100

    remove comments and additional spaces

--- minimum-swaps-2.js
+++ minimum-swaps-2.js
@@ -2,12 +2,11 @@
 
 let testArray = [1, 3, 5, 2, 4, 6, 7];
 
-
 function minimumSwaps(list) {
   let counter = 0;
   let indexOfSmallest = 0;
   let temp = 0;
-  
+
   for (let i = 0; i < list.length; i++) {
     indexOfSmallest = list.indexOf(i+1);
     if (list[i] > i + 1) {
@@ -22,11 +21,5 @@ function minimumSwaps(list) {
 
 console.log(minimumSwaps(testArray));
 
-//           [4, 3, 1, 2]
-// [4 <-> 1] [1, 3, 4, 2]
-// [3 <-> 2] [1, 2, 4, 3]
-// [4 <-> 3] [1, 2, 3, 4]
-
-// 3
 
 
