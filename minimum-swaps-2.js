ef6c69d4447b4f25a03578e07c6726cae8f537bb
Nora <norapuskas@yahoo.com>
2018-11-17 14:50:55 +0100

    add better solution without indexOf

--- minimum-swaps-2.js
+++ minimum-swaps-2.js
@@ -2,24 +2,26 @@
 
 let testArray = [1, 3, 5, 2, 4, 6, 7];
 
+function swap(list, index1, index2) {
+  let temp = list[index1];
+  list[index1] = list[index2];
+  list[index2] = temp;
+
+  return list;
+}
+
 function minimumSwaps(list) {
   let counter = 0;
-  let indexOfSmallest = 0;
-  let temp = 0;
 
-  for (let i = 0; i < list.length; i++) {
-    indexOfSmallest = list.indexOf(i+1);
+  for (let i = 0; i < list.length; ) {
     if (list[i] > i + 1) {
-      temp = list[i];
-      list[i] = list[indexOfSmallest];
-      list[indexOfSmallest] = temp;   
+      swap(list, i, list[i] - 1);
       counter++;
+    } else {
+      i++;
     }
   }
   return counter;
 }
 
 console.log(minimumSwaps(testArray));
-
-
-
