fae63aff9ada67de1ef7935f738e4f94d12e665b
Nora <norapuskas@yahoo.com>
2018-11-18 23:16:24 +0100

    refactor solution

--- very-big-sum.js
+++ very-big-sum.js
@@ -3,11 +3,8 @@
 let testArray = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
 
 function getSum(list) {
-  let result = 0;
-  for (let i = 0; i < list.length; i++) {
-    result += list[i];
-  }
-  return result;
+  
+  return list.reduce((a,b) => a + b);
 }
 console.log(getSum(testArray));
 
