20df74ca6d2fc26cb7b1d831cd199de7b7fc7c6c
Nora <norapuskas@yahoo.com>
2018-11-20 00:26:54 +0100

    refactor, change if statement to ternary operator

--- grading-students.js
+++ grading-students.js
@@ -4,10 +4,7 @@ let originalGrades = [73, 67, 38, 33];
 
 function changeGrades(grades) {
   return grades.map((grade) => {
-    if (grade >= 38 && grade % 5 >= 3) {
-      grade += 5 - (grade % 5);
-    }
-    return grade;
+    grade >= 38 && grade % 5 >= 3 ? grade + 5 - (grade % 5) : grade;
   });
 }
 
