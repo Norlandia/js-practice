66c8645c0e5971d66a2f2e2fad73479afd331a90
Nora <norapuskas@yahoo.com>
2018-11-20 00:20:55 +0100

    refactor again, remove additional if statement

--- grading-students.js
+++ grading-students.js
@@ -4,10 +4,8 @@ let originalGrades = [73, 67, 38, 33];
 
 function changeGrades(grades) {
   for (let i = 0; i < grades.length; i++) {
-    if (grades[i] >= 38) {
-      if (grades[i] % 5 >= 3) {
-        grades[i] += 5 - (grades[i] % 5);
-      }
+    if (grades[i] >= 38 && grades[i] % 5 >= 3) {
+      grades[i] += 5 - (grades[i] % 5);
     }
     return grades;
   }
