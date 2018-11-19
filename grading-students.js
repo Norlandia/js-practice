e2579e2ad3512961911ea81dcd37a3ce12b2baa0
Nora <norapuskas@yahoo.com>
2018-11-20 00:19:43 +0100

    refactor solution

--- grading-students.js
+++ grading-students.js
@@ -5,15 +5,12 @@ let originalGrades = [73, 67, 38, 33];
 function changeGrades(grades) {
   for (let i = 0; i < grades.length; i++) {
     if (grades[i] >= 38) {
-      if (grades[i] % 5 === 3) {
-        grades[i] += 2;
-      } else if (grades[i] % 5 === 4) {
-        grades[i] += 1;
+      if (grades[i] % 5 >= 3) {
+        grades[i] += 5 - (grades[i] % 5);
       }
     }
+    return grades;
   }
-  return grades;
 }
 
 console.log(changeGrades(originalGrades));
-// 0 0 0 2 1 0 0 0 2 1 
