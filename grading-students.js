41e32e87c84d0794971eac26f28ed8fede641c97
Nora <norapuskas@yahoo.com>
2018-11-20 00:29:17 +0100

    fix, remove curly braces

--- grading-students.js
+++ grading-students.js
@@ -3,9 +3,9 @@
 let originalGrades = [73, 67, 38, 33];
 
 function changeGrades(grades) {
-  return grades.map((grade) => {
-    grade >= 38 && grade % 5 >= 3 ? grade + 5 - (grade % 5) : grade;
-  });
+  return grades.map((grade) =>
+    grade >= 38 && grade % 5 >= 3 ? grade + 5 - (grade % 5) : grade
+  );
 }
 
 console.log(changeGrades(originalGrades));
