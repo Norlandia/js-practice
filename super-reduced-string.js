265863bc7b05f2c2642aaef6c616106a25990be7
Nora <norapuskas@yahoo.com>
2018-11-22 00:05:16 +0100

    add solution to super-reduced-string exercise

---
+++ super-reduced-string.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let testString = 'baab';
+
+function superReducedString(string) {
+  let newString = '';
+
+  string = string.split('');
+  string.sort(...string);
+  console.log(string);
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== string[i + 1]) {
+      newString += string[i];
+    } else {
+      i++;
+    }
+  }
+
+  return newString ? newString : 'Empty String';
+}
+
+console.log(superReducedString(testString));
