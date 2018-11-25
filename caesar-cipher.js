ee358110a17f5d5d25eb9bac0744831bc0533e6c
Nora <norapuskas@yahoo.com>
2018-11-25 23:27:10 +0100

    fix counting method in the alphabet

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,28 +1,33 @@
 'use strict';
 
-let testString = 'middle-Outz';
-let k = 2;
+let testString = 'www.abc.xy';
+let k = 87;
 
 function caesarCipher(string, shift) {
   let newString = '';
 
   for (let i = 0; i < string.length; i++) {
+
     if (string[i].charCodeAt() >= 97 && string[i].charCodeAt() <= 122) {
+
       if (string[i].charCodeAt() + shift > 122) {
         newString += String.fromCharCode(
-          ((string[i].charCodeAt() + shift) % 123) + 97
+          ((string[i].charCodeAt() - 97 + shift) % 26) + 97
         );
       } else {
         newString += String.fromCharCode(string[i].charCodeAt() + shift);
       }
+
     } else if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90) {
+
       if (string[i].charCodeAt() + shift > 90) {
         newString += String.fromCharCode(
-          ((string[i].charCodeAt() + shift) % 91) + 65
+          ((string[i].charCodeAt() - 65 + shift) % 26) + 65
         );
       } else {
         newString += String.fromCharCode(string[i].charCodeAt() + shift);
       }
+
     } else {
       newString += string[i];
     }
@@ -30,4 +35,3 @@ function caesarCipher(string, shift) {
   return newString;
 }
 console.log(caesarCipher(testString, k));
-
