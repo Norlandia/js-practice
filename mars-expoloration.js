ea4be9febd9cada492e8b77658a6e4cfefbd6f88
Nora <norapuskas@yahoo.com>
2018-11-26 17:01:52 +0100

    remove comments

--- mars-expoloration.js
+++ mars-expoloration.js
@@ -23,9 +23,3 @@ function marsExploration(s) {
 }
 
 console.log(marsExploration(sosString));
-
-// S O S   S O S   S O S  length = 9 {9/3 = 3 SOS}
-// 0 1 2   3 4 5   6 7 8
-//6s 3o
-
-//4sos.length = 12; o = s.length / 3 = 4,  s = s.length / 3 * 2
