87e80be79ea0cae5df95872538866d40b762bddc
Nora <norapuskas@yahoo.com>
2018-11-28 00:21:37 +0100

    refactor solution

--- icecream-parlor.js
+++ icecream-parlor.js
@@ -4,15 +4,13 @@ let flavorsList = [1, 4, 5, 3, 2];
 let money = 4;
 
 function icecreamParlor(prices, m) {
-  let result = [];
   for (let i = 0; i < prices.length; i++) {
     for (let j = i + 1; j < prices.length; j++) {
       if (prices[i] + prices[j] === m) {
-        result.push(i + 1, j + 1);
+        return [i + 1, j + 1];
       }
     }
   }
-  return result;
 }
 console.log(icecreamParlor(flavorsList, money));
 
