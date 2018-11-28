0dba7ed46dd2e895b443052983c4640be1567a33
Nora <norapuskas@yahoo.com>
2018-11-28 02:29:02 +0100

    add solution to gemstones exercise

---
+++ gemstones.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let rocks = ['abcdde', 'baccd', 'eeabg'];
+
+const gemstones = (list) => {
+  let gemstones = 0;
+  let letterObject = {};
+
+  for (let letter of list[0]) {
+    if (!letterObject[letter]) {
+      letterObject[letter] = 1;
+    }
+  }
+
+  for (let i = 1; i < list.length; i++) {
+    let mySet = new Set([...list[i]]);
+    for (let letter of mySet) {
+      if (letterObject[letter]) {
+        letterObject[letter]++;
+      }
+    }
+  }
+
+  for (let value of Object.values(letterObject)) {
+    value === list.length ? gemstones++ : gemstones;
+  }
+  return gemstones;
+};
+console.log(gemstones(rocks));
