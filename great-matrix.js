6cb90181712e2febb68977fd7abce6b24613abdd
Nora <norapuskas@yahoo.com>
2018-12-01 15:40:41 +0100

    add solution to ruep great-matrix exercise

---
+++ great-matrix.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let matrix1 = [[2, 1], [0, 1]];
+
+let matrix2 = [[0, 3], [-1, 1]];
+
+const greatMatrix = (m1, m2) => {
+  let result = [];
+
+  for (let i = 0; i < m1.length; i++) {
+    let smallLists = [];
+    for (let j = 0; j < m1[i].length; j++) {
+      if (m1[i][j] > m2[i][j]) {
+        smallLists.push(m1[i][j]);
+      } else {
+        smallLists.push(m2[i][j]);
+      }
+    }
+    result.push(smallLists);
+  }
+  return result;
+};
+
+console.log(greatMatrix(matrix1, matrix2));
+
+// return ===> [ [2, 3], [0,1]]
