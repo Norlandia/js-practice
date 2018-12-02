46ef2c8c6d4edd40d5697d4fb0e3513377570784
Nora <norapuskas@yahoo.com>
2018-12-02 23:54:43 +0100

    add soolution to exercise

--- day5-arrow-functions.js
+++ day5-arrow-functions.js
@@ -2,6 +2,11 @@
 
 let numbers = [1, 2, 3, 4, 5];
 
-const modifyArray = (nums) => {};
+const modifyArray = (nums) => {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] % 2 === 0 ? (nums[i] *= 2) : (nums[i] *= 3);
+  }
+  return nums;
+};
 
 console.log(modifyArray(numbers));
