e3870bdd97557879cd9d00c08e7f2dc015d90a4f
Nora <norapuskas@yahoo.com>
2018-12-05 18:22:54 +0100

    add solution to migratory-birds exercise

---
+++ migratory-birds.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let birds = [1, 1, 1, 4, 4, 4, 5, 3];
+
+const migratoryBirds = (birdList) => {
+  let birdFrequency = {};
+
+  for (let bird of birdList) {
+    birdFrequency[bird] ? ++birdFrequency[bird] : (birdFrequency[bird] = 1);
+  }
+
+  let birdEntries = Object.entries(birdFrequency);
+  let result = birdEntries[0];
+
+  for (let i = 1; i < birdEntries.length; i++) {
+    if (birdEntries[i][1] > result[1]) {
+      result = birdEntries[i];
+    } else if (birdEntries[i][1] === result[1]) {
+      if (birdEntries[i][0] < result[0]) {
+        result = birdEntries[i];
+      }
+    }
+  }
+
+  return result[0];
+};
+
+console.log(migratoryBirds(birds));
