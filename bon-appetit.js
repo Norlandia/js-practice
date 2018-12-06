38cddd424f7484913cd72c2031951bbeb0d6bb7a
Nora <norapuskas@yahoo.com>
2018-12-06 15:41:22 +0100

    refactor solution, add map and ternary operator, remove console.log from end

--- bon-appetit.js
+++ bon-appetit.js
@@ -2,21 +2,15 @@
 
 let fullBill = [3, 10, 2, 9];
 let priceNotIncl = 1;
-let refund = 12;
+let refund = 7;
 
 const bonAppetit = (bill, notInclIndex, charged) => {
   let annaBill = 0;
-  for (let i = 0; i < bill.length; i++) {
-    annaBill += bill[i] / 2;
-  }
+
+  bill.map((price, i) => (annaBill += bill[i] / 2));
   annaBill -= bill[notInclIndex] / 2;
 
-  if (charged === annaBill) {
-    console.log('Bon Appetit');
-  } else if (charged > annaBill) {
-    console.log(charged - annaBill);
-  }
+  charged === annaBill ? console.log('Bon Appetit') : console.log(charged - annaBill);
 };
 
-console.log(bonAppetit(fullBill, priceNotIncl, refund));
-
+bonAppetit(fullBill, priceNotIncl, refund);
