8155da4342a02f78b2a0516f89292b7d1cdcf07e
Nora <norapuskas@yahoo.com>
2018-12-06 23:34:06 +0100

    add solution to electronics-shop exercise

---
+++ electronics-shop.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let keyboardPrices = [3, 1];
+let drivePrices = [5, 2, 8];
+let budget = 10;
+
+const getMoneySpent = (keyboards, drives, money) => {
+  let highestPrice = 0;
+  keyboards.sort((a, b) => b - a);
+  drives.sort((a, b) => b - a);
+
+  if (keyboards[0] > drives[0]) {
+    highestPrice = keyboards[0];
+    for (let i = 0; i < drives.length; i++) {
+      if (highestPrice + drives[i] <= money) {
+        console.log(highestPrice + drives[i]);
+      }
+    }
+  } else {
+    highestPrice = drives[0];
+    for (let i = 0; i < keyboards.length; i++) {
+      if (highestPrice + keyboards[i] <= money) {
+        console.log(highestPrice + keyboards[i]);
+      }
+    }
+  }
+};
+getMoneySpent(keyboardPrices, drivePrices, budget);
