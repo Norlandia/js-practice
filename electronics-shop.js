f1ae2ad2b87705ab896066b69d10dc94b06b6cd9
Nora <norapuskas@yahoo.com>
2018-12-07 00:16:27 +0100

    fix solution, to check all possible matches

--- electronics-shop.js
+++ electronics-shop.js
@@ -5,25 +5,17 @@ let drivePrices = [5, 2, 8];
 let budget = 10;
 
 const getMoneySpent = (keyboards, drives, money) => {
-  let highestPrice = 0;
-  keyboards.sort((a, b) => b - a);
-  drives.sort((a, b) => b - a);
-
-  if (keyboards[0] > drives[0]) {
-    highestPrice = keyboards[0];
-    for (let i = 0; i < drives.length; i++) {
-      if (highestPrice + drives[i] <= money) {
-        return highestPrice + drives[i];
-      }
-    }
-  } else {
-    highestPrice = drives[0];
-    for (let i = 0; i < keyboards.length; i++) {
-      if (highestPrice + keyboards[i] <= money) {
-        return highestPrice + keyboards[i];
+  let highestPair = -1;
+  for (let i = 0; i < keyboards.length; i++) {
+    for (let j = 0; j < drives.length; j++) {
+      if (
+        keyboards[i] + drives[j] <= money &&
+        highestPair < keyboards[i] + drives[j]
+      ) {
+        highestPair = keyboards[i] + drives[j];
       }
     }
   }
-  return -1;
+  return highestPair;
 };
 console.log(getMoneySpent(keyboardPrices, drivePrices, budget));
