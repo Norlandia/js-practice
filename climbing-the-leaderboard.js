1ba90eee293ea12ca7e00d000ff925620d0340d5
Nora <norapuskas@yahoo.com>
2018-12-09 17:55:44 +0100

    solve exercise with binary search and recursion

--- climbing-the-leaderboard.js
+++ climbing-the-leaderboard.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let leaderboardScores = [100, 100, 50, 40, 40, 20, 10];
-let aliceScores = [5, 25, 50, 120];
+let leaderboardScores = [100, 90, 90, 80, 75, 60];
+let aliceScores = [50, 65, 77, 90, 102];
 
 const climbingLeaderboard = (leaderboard, alice) => {
   let result = [];
@@ -37,4 +37,38 @@ const climbingLeaderboard2 = (leaderboard, alice) => {
   return result;
 };
 
-console.log(climbingLeaderboard2(leaderboardScores, aliceScores));
+
+
+const binarySearch = (arr, i, score) => {
+  let c = Math.floor(arr.length / 2);
+
+  if (arr[c] === score) {
+    return i + 1;
+  } else if (arr.length === 1) {
+    if (arr[0] > score) {
+      return i + 1;
+    } else {
+      return i;
+    }
+  } else if (arr[c] > score) {
+    return binarySearch(arr.slice(c), i + Math.ceil(c / 2), score);
+  } else {
+    return binarySearch(arr.slice(0, c), i - Math.floor(c / 2), score);
+  }
+};
+
+const climbingLeaderboard3 = (leaderboard, alice) => {
+  let result = [];
+
+  leaderboard = [...new Set(leaderboard)];
+
+  for (let score of alice) {
+    result.push(
+      binarySearch(leaderboard, Math.floor(leaderboard.length / 2), score)
+    );
+  }
+
+  return result;
+};
+console.log(climbingLeaderboard3(leaderboardScores, aliceScores));
+
