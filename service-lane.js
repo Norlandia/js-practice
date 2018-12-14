37c51693da52060e92494aabcad57e25f5bd522d
Nora <norapuskas@yahoo.com>
2018-12-14 21:30:55 +0100

    add solution to service-lane exercise

--- service-lane.js
+++ service-lane.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let testArray = [2, 3, 1, 2, 3, 2, 3, 3];
-let cases = [4, 6];
+let cases = [0, 3];
 
 const serviceLane = (roadWidth, inOut) => {
   roadWidth = roadWidth.slice(inOut[0], inOut[1] + 1);
