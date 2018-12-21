3e33833367d6a227c2ee144921a269b00865e421
Nora <norapuskas@yahoo.com>
2018-12-21 20:40:47 +0100

    fix, but still needs some changes

--- library-fine.js
+++ library-fine.js
@@ -1,24 +1,39 @@
 'use strict';
 
-const libraryFine = (rd1, rm1, ry1, dd2, dm2, dy2) => {
+const libraryFine = (rd, rm, ry, dd, dm, dy) => {
+  // if (dd >= rd && dm >= rm && dy >= ry) {
+  //   return 0;
+  // }
 
-    if (dd2 >= rd1 && dm2 >= rm1 && dy2 >= ry1) {
-      return 0;
-    }
-    if (dy2 < ry1) {
-      return 10000;
-    } else if (dm2 < rm1) {
-      return (rm1 - dm2) * 500;
-    } else if (dd2 < rd1) {
-      return (rd1 - dd2) * 15;
-    }
+  if (dy < ry) {
+    return 10000;
+  } else if (dm < rm) {
+    return (rm - dm) * 500;
+  } else if (dd < rd) {
+    return (rd - dd) * 15;
+  }
+
+  if (dy > ry) {
+    return 0;
+  } else if (dm > rm) {
+    return 0;
+  } else if (dd > rd) {
+    return 0;
+  } else if (dy === ry && dm === rm && dd === rd) {
+    return 0;
+  }
 };
-console.log(libraryFine(9, 6, 2015, 6, 6, 2015));
+console.log(libraryFine(2, 7, 1014, 1, 1, 1015));
 
-// before due --> 0 
+// before due --> 0
 
 // after bnut same month and year --> 15 * day difference
 
 // another month but still the same year --> 500 * month difference
 
 // another year --> 10000
+
+// FIX IF THE BOOK WAS GIVEN BACK BEFORE DUE DATE
+
+//returned
+//due
