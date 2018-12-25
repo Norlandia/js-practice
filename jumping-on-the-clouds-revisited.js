dc0a766401ff4f81ed3740b13f4188f52c5eddae
Nora <norapuskas@yahoo.com>
2018-12-25 16:08:03 +0100

    refactor solution, change if statements to ternary

--- jumping-on-the-clouds-revisited.js
+++ jumping-on-the-clouds-revisited.js
@@ -7,12 +7,8 @@ const jumpingOnClouds = (jumpSize, clouds) => {
   let energy = 100;
 
   for (let i = 0; (i + jumpSize) % clouds.length; i++) {
-    energy -= 1;
-    if (clouds[i + jumpSize] === 1) {
-      energy -= 2;
-    }
+    clouds[i + jumpSize] === 1 ? (energy -= 3) : (energy -= 1);
     i += jumpSize - 1;
-    console.log(energy);
   }
   clouds[0] === 1 ? (energy -= 3) : (energy -= 1);
   return energy;
